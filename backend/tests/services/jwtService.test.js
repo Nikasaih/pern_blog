1097010a028b0a0c96ce8c12dce055b5fb260b65
Nikasaih <gchaducnguyen@gmail.com>
2022-03-13 19:36:32 +0100

    feat: implementation base file for test

---
+++ backend/tests/services/jwtService.test.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+describe("jwtService", () => {
+  it("test", () => {
+    const a = 2;
+    expect(a).toBe(2);
+  });
+  it("test", () => {
+    const a = 4;
+    expect(a).toBe(4);
+  });
+});
