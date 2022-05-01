db56611f9a044eb26759bf790e2de1e8bc08c35c
Nikasaih <gchaducnguyen@gmail.com>
2022-05-01 15:22:35 +0200

    feat: request (banUser, suspendUser, unSuspendUser)

--- front/api/requests/userRequest.js
+++ front/api/requests/userRequest.js
@@ -1,4 +1,11 @@
-import { registerRoute, signInRoute } from "../routes/userRoutes.js";
+import {
+  registerRoute,
+  signInRoute,
+  deleteMyAccountRoute,
+  suspendUserByIdRoute,
+  unSuspendUserByIdRoute,
+  banUserByIdRoute,
+} from "../routes/userRoutes.js";
 import axios from "axios";
 
 export const registerRequest = async ({ password, email, displayName }) => {
@@ -8,4 +15,31 @@ export const registerRequest = async ({ password, email, displayName }) => {
 export const signInRequest = async ({ email, password }) => {
   const response = await axios.post(signInRoute, { email, password });
   localStorage.setItem("jwt", response.data);
+};
+
+export const deleteMyAccountRequest = () => {
+  return axios.delete(deleteMyAccountRoute);
+};
+
+export const suspendUserRequest = (id) => {
+  const headers = getAuthHeader();
+  return axios.post(suspendUserByIdRoute(id), {}, headers);
+};
+
+export const unSuspendUserRequest = (id) => {
+  const headers = getAuthHeader();
+  return axios.post(unSuspendUserByIdRoute(id), {}, headers);
+};
+
+export const banUserRequest = (id) => {
+  const headers = getAuthHeader();
+  return axios.post(banUserByIdRoute(id), {}, headers);
+};
+
+const getAuthHeader = () => {
+  const auth = localStorage.getItem("jwt");
+  if (!auth) {
+    return;
+  }
+  return { authentication: auth };
 };
--- front/api/routes/userRoutes.js
+++ front/api/routes/userRoutes.js
@@ -4,12 +4,12 @@ export const registerRoute = rootRoute + "/register";
 export const signInRoute = rootRoute + "/sign-in";
 export const deleteMyAccountRoute = rootRoute + "/delete-my-account";
 
-export const suspendUserById = (id) => {
+export const suspendUserByIdRoute = (id) => {
   return `${rootRoute}/suspend-user/${id}`;
 };
-export const unSuspendUserById = (id) => {
+export const unSuspendUserByIdRoute = (id) => {
   return `${rootRoute}/unsuspend-user/${id}`;
 };
-export const banUserById = (id) => {
+export const banUserByIdRoute = (id) => {
   return `${rootRoute}/ban-user/${id}`;
 };
